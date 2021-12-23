c68ef7a998934e7cc9f0b08d6061810c1d939302
Imran <89724435+Huseynlimran@users.noreply.github.com>
2021-12-23 20:56:08 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,82 @@
-const nav = document.querySelector(".nav-links")
-const burger = document.querySelector(".burger")
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
 
-burger.addEventListener("click", () =>{
-    nav.classList.toggle("nav-active")
-    burger.classList.toggle("toggle")
-})
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+});
 
-const cursor = document.querySelector('.cursor');
+const cursor = document.querySelector(".cursor");
 
-        document.addEventListener('mousemove', e => {
-            cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
-        })
+document.addEventListener("mousemove", (e) => {
+  cursor.setAttribute(
+    "style",
+    "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+  );
+});
 
-        document.addEventListener('click', () => {
-            cursor.classList.add("expand");
+document.addEventListener("click", () => {
+  cursor.classList.add("expand");
 
-            setTimeout(() => {
-                cursor.classList.remove("expand");
-            }, 500)
-        })
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500);
+});
 
+ScrollReveal().reveal(".top", {
+  origin: "top",
+  duration: 2000,
+  distance: "5rem",
+  interval: 100,
+  opacity: 0,
+});
+ScrollReveal().reveal(".bottom", {
+  origin: "bottom",
+  duration: 2000,
+  distance: "5rem",
+  opacity: 0,
+});
+ScrollReveal().reveal(".left", {
+  origin: "left",
+  duration: 2000,
+  distance: "5rem",
+  opacity: 0,
+});
 
-        ScrollReveal().reveal('.top',{
-            origin: 'top' , duration: 2000, distance: '5rem', interval:100, opacity : 0
-        })
-        ScrollReveal().reveal('.bottom',{
-            origin: 'bottom' , duration: 2000, distance: '5rem',opacity : 0
-        })
-        ScrollReveal().reveal('.left',{
-            origin: 'left' , duration: 2000, distance: '5rem', opacity : 0
-        })
-        
-        ScrollReveal().reveal('.sag',{
-            origin: 'right' , duration: 2000, distance: '5rem',  interval:300, opacity : 0
-        })
-        ScrollReveal().reveal('.sol',{
-            origin: 'left' , duration: 1000, distance: '1rem',  interval:200, opacity : 0
-        })
+ScrollReveal().reveal(".sag", {
+  origin: "right",
+  duration: 2000,
+  distance: "5rem",
+  interval: 300,
+  opacity: 0,
+});
+ScrollReveal().reveal(".sol", {
+  origin: "left",
+  duration: 1000,
+  distance: "1rem",
+  interval: 200,
+  opacity: 0,
+});
 
-
-        const tl = gsap.timeline({default: {ease: ''}})
-        tl.fromTo('nav',{opacity :0 ,y: "-100%"}, {y: "0%", opacity:1, duration: 1})
-        tl.from('.home .txt',{opacity :0,x:"-100%",duration:.6})
-        tl.from('.home .txt2',{x:"-100%",opacity :0,duration:1})
-        tl.from('.home span',{x:"-100%",opacity :0,duration:1})
-        tl.from('.home .txt3',{x:"-100%",opacity :0,duration:1})
-        tl.from('.home a',{x:"-100%",opacity :0,duration:.6})
-
-       
-        
-
+const tl = gsap.timeline({ default: { ease: "" } });
+tl.fromTo(
+  "nav",
+  { opacity: 0, y: "-100%" },
+  { y: "0%", opacity: 1, duration: 1 }
+);
+tl.from(".home .txt", { opacity: 0, x: "-100%", duration: 0.6 });
+tl.from(".home .txt2", { x: "-100%", opacity: 0, duration: 1 });
+tl.from(".home span", { x: "-100%", opacity: 0, duration: 1 });
+tl.from(".home .txt3", { x: "-100%", opacity: 0, duration: 1 });
+tl.from(".home a", { x: "-100%", opacity: 0, duration: 0.6 });
+gsap.fromTo(
+  ".sf",
+  { x: "500", opacity: 0, rotation: "-120deg" },
+  {
+    ease: "elastic.out(1, 0.4)",
+    opacity: 1,
+    x: "0",
+    duration: 3,
+    rotation: "0deg",
+  }
+);
