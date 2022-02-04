b9a756050d13a0ee1bd5f2771f09dbde25896b6a
Imran <89724435+Huseynlimran@users.noreply.github.com>
2022-02-04 22:22:55 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,34 @@ window.addEventListener("mousemove", (evt) => {
   eye2.style.transform = `translateY(${y}px) translateX(${x}px)`;
 });
 
+const nav = document.querySelector(".nav-links");
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
+  burger.classList.toggle("toggle");
+
+});
+
+// const cursor = document.querySelector(".cursor");
+// document.addEventListener("click", () => {
+//   cursor.classList.add("expand");
+
+//   setTimeout(() => {
+//     cursor.classList.remove("expand");
+//   }, 500);
+// });
+
+// document.addEventListener("mousemove", (e) => {
+//  cursor.setAttribute(
+//     "style",
+//     "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
+//    );
+//  });
+
+// animation
+
 // intro
 
 const sg = gsap.timeline({ default: { ease: "power1.out" } });
@@ -32,7 +60,24 @@ sg.to(".text", { y: "0%", duration: 1.5, stagger: 0.25 });
 sg.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
 sg.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 sg.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-sg.fromTo(".mensi", { opacity: 0 }, { opacity: 1, duration: 1 },"-=1");
+sg.fromTo(
+  ".linksNav",
+  { y: "-50%", opacity: 0 },
+  { y: "0%", opacity: 1, stagger: 0.3 },
+  "-=1"
+);
+sg.fromTo(
+  ".mensi1",
+  { "border-top": "200px solid black", opacity: 0, y: "-15%" },
+  { "border-top": "40px solid black", opacity: 1, duration: 1, y: "0%" },
+  "-=1"
+);
+sg.fromTo(
+  ".mensi2",
+  { "border-top": "200px solid black", opacity: 0, y: "-15%" },
+  { "border-top": "40px solid black", opacity: 1, duration: 1, y: "0%" },
+  "-=1"
+);
 
 const boxe = document.querySelectorAll(".boxoso #layer");
 boxe.forEach((element) => {
@@ -52,30 +97,6 @@ boxe.forEach((element) => {
     }, 300);
   });
 });
-
-const nav = document.querySelector(".nav-links");
-const burger = document.querySelector(".burger");
-
-burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
-  burger.classList.toggle("toggle");
-});
-
-// const cursor = document.querySelector(".cursor");
-// document.addEventListener("click", () => {
-//   cursor.classList.add("expand");
-
-//   setTimeout(() => {
-//     cursor.classList.remove("expand");
-//   }, 500);
-// });
-
-// document.addEventListener("mousemove", (e) => {
-//  cursor.setAttribute(
-//     "style",
-//     "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;"
-//    );
-//  });
 
 ScrollReveal().reveal(".top", {
   origin: "top",
@@ -112,20 +133,6 @@ ScrollReveal().reveal(".sol", {
   interval: 100,
   opacity: 0,
 });
-
-const tl = gsap.timeline({ default: { ease: "" } });
-
-gsap.fromTo(
-  ".sf",
-  { x: "500", opacity: 0, rotation: "-120deg" },
-  {
-    ease: "elastic.out(1, 0.4)",
-    opacity: 1,
-    x: "0",
-    duration: 3,
-    rotation: "0deg",
-  }
-);
 
 // let path= document.querySelector("path")
 // let pathLength = path.getTotalLength()
